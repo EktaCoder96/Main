aa623ff348a8bd0d84a5a5dbe237c30d09db11cd
ekta parmar <parmaraditya07@gmail.com>
2019-06-14 21:13:53 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TryComponent } from './try/try.component';
+import { DashboardComponent } from './components/login-overview/dashboard.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { ServiceesComponent } from './servicees/servicees.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'dashboard', component: DashboardComponent, pathMatch: 'full' },
+  { path: 'gotoekta', component: TryComponent },
+  { path: 'login', component: DashboardComponent },
+  { path: 'adminlogin', component: AdminloginComponent},
+  { path: 'restaurant', component: RestaurantComponent},
+  { path: 'servicees', component: ServiceesComponent},
+  { path: '', component: HomeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TryComponent } from './try/try.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginModule } from './modules/login/login.module';
+import { DesignModule } from './modules/design/design.module';
+import { LoginDialogComponent, RegisterDialogComponent, DashboardComponent } from './components/login-overview/dashboard.component';
+
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { ServiceesComponent } from './servicees/servicees.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CreateUserComponent } from './components/create-user/create-user.component';
+import { CreateHotelComponent } from './components/create-hotel/create-hotel.component';
+import { CreateRoomComponent } from './components/create-room/create-room.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    TryComponent,
+    DashboardComponent,
+    ServiceesComponent,
+    RestaurantComponent,
+    AdminloginComponent,
+    HomeComponent,
+    CreateUserComponent,
+    CreateHotelComponent,
+    CreateRoomComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    LoginModule,
+    DesignModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [LoginDialogComponent, RegisterDialogComponent]
 })
 export class AppModule { }
+
